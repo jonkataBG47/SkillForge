f89533f98b9057f45ec2851e0d34ec61a6abd0d8
Yoan Dimitrov <joan13151412@gmail.com>
2026-02-12 15:50:15 +0200

    new features

--- resources/views.py
+++ resources/views.py
@@ -3,6 +3,7 @@ from resources.models import Resource
 from django.db.models import Count
 from resources.forms import FormResource
 from django.http import HttpRequest, HttpResponse
+from core.forms import SearchForm
 def create_resource(request:HttpRequest):
     form = FormResource(request.POST or None)
     if request.method == 'POST' and form.is_valid():
@@ -27,8 +28,12 @@ def delete_resource(request:HttpRequest,id):
     context = {'form':form,'resource':resource}
     return render(request,'resources/resource_delete_confirm.html',context)
 def resource_list(request:HttpRequest):
-    resources = Resource.objects.all().order_by('-updated_at','title')
-    context = {'resources':resources}
+    form = SearchForm(request.GET or None)
+    resource = Resource.objects.all().order_by('-updated_at','title')
+    if request.method == 'GET' and form.is_valid():
+        query = form.cleaned_data['query']
+        resource = Resource.objects.filter(title__icontains=query)
+    context = {'form':form,'resources':resource}
     return render(request,'resources/resource_list.html',context)
 def resource_detail(request:HttpRequest,slug):
     resource = get_object_or_404(Resource,slug=slug)
