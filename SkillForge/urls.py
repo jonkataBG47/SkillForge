a79532e313f95c91e6715c842bd35085c07e5614
Yoan Dimitrov <joan13151412@gmail.com>
2026-02-13 11:01:10 +0200

    fix errors

--- SkillForge/urls.py
+++ SkillForge/urls.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 URL configuration for SkillForge project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,15 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings 
+from django.urls import path, include, re_path
+from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('', include('skills.urls')),
     path('', include('learning_paths.urls')),
     path('', include('resources.urls')),
-    path('',include('category.urls'))
+    path('', include('category.urls')),
 ]
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
+else:
+    urlpatterns += [re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATICFILES_DIRS[0]})]
