159f60f481d9103a236abf7d08a1d44e1265581c
Yoan Dimitrov <joan13151412@gmail.com>
2026-04-16 16:25:35 +0300

    test

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -9,6 +9,7 @@ def send_register_email(email, username):
         f'Hello {username} and welcome to SkillForge! Your registration was successful.',
         EMAIL_HOST_USER,
         [email],
+        fail_silently=False
     )
 
 def send_register_email_async(email, username):
